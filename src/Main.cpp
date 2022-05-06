58e096e58cd363cd41de36cc59901ba2b0c8ef98
Sumant Kalra <57260000+sumant-kalra@users.noreply.github.com>
2022-05-07 00:35:24 +0530

    chg: Learning Recursion

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,45 +1,33 @@
-#include <iostream>
-#include <cstdlib>
+#define MAIN 1
+#if MAIN
 
-#include "Recursion.h"
+#include <iostream>
+
+void func1(int n);
+void func2(int n);
 
 int main(int argc, char *argv[])
 {
-#if 0
-    // Factorial
-    std::cout << recursion::factorial_r(atoll(argv[1])) << "\n";
-    std::cout << recursion::factorial_i(atoll(argv[1])) << "\n";
-
-    // Palindrome
-    std::cout << recursion::isPalindrome_r(std::string(argv[1])) << "\n";
-    std::cout << recursion::isPalindrome_i(std::string(argv[1])) << "\n";
-
-    // Power function
-    std::cout << recursion::powerTrivia_r(atof(argv[1]), atoi(argv[2])) << "\n";
-    std::cout << recursion::powerOptimized_r(atof(argv[1]), atoi(argv[2])) << "\n";
-    std::cout << recursion::powerTrivia_i(atof(argv[1]), atoi(argv[2])) << "\n";
-    std::cout << recursion::powerOptimized_i(atof(argv[1]), atoi(argv[2])) << "\n";
-
-
-    // Print function
-    recursion::printN0N_i(atoi(argv[1]));
-    recursion::printN0N_r(atoi(argv[1]));
-
-    // Fibonacci
-    std::cout << recursion::fibonacciNumber_r(atoi(argv[1])) << "\n";
-    std::cout << recursion::fibonacciNumber_i(atoi(argv[1])) << "\n";
-
-    std::cout << recursion::power_i(atof(argv[1]), atoi(argv[2])) << "\n";
-    std::cout << recursion::power_r(atof(argv[1]), atoi(argv[2])) << "\n";
-#endif
-
-    std::vector<int> myVec{-98, -39, -1, 3, 7, 13, 89, 109, 133, 180, 389};
-
-    std::array<int, 11> myArray{-98, -39, -1, 3, 7, 13, 89, 109, 133, 180, 389};
-
-    std::cout << recursion::binarySearch_i<11>(myArray, atoi(argv[1])) << "\n";
-
-    std::cout << recursion::binarySearch_r(myVec, atoi(argv[1])) << "\n";
-
+    func1(3);
+    func2(3);
     return 0;
 }
+
+void func1(int n)
+{
+    if (n > 0)
+    {
+        std::cout << n << "\n";
+        func1(n - 1);
+    }
+}
+
+void func2(int n)
+{
+    if (n > 0)
+    {
+        func2(n - 1);
+        std::cout << n << "\n";
+    }
+}
+#endif
--- src/Recursion.cpp
+++ src/Recursion.cpp
@@ -282,3 +282,55 @@ bool recursion::binarySearch_r(const std::vector<int> &vec, int num)
         }
     }
 }
+
+// --------------------------------------------------------------------------- //
+#define MAIN_RECURSION 0
+#if MAIN_RECURSION
+
+#include <iostream>
+#include <cstdlib>
+
+#include "Recursion.h"
+
+int main(int argc, char *argv[])
+{
+#if 0
+    // Factorial
+    std::cout << recursion::factorial_r(atoll(argv[1])) << "\n";
+    std::cout << recursion::factorial_i(atoll(argv[1])) << "\n";
+
+    // Palindrome
+    std::cout << recursion::isPalindrome_r(std::string(argv[1])) << "\n";
+    std::cout << recursion::isPalindrome_i(std::string(argv[1])) << "\n";
+
+    // Power function
+    std::cout << recursion::powerTrivia_r(atof(argv[1]), atoi(argv[2])) << "\n";
+    std::cout << recursion::powerOptimized_r(atof(argv[1]), atoi(argv[2])) << "\n";
+    std::cout << recursion::powerTrivia_i(atof(argv[1]), atoi(argv[2])) << "\n";
+    std::cout << recursion::powerOptimized_i(atof(argv[1]), atoi(argv[2])) << "\n";
+
+
+    // Print function
+    recursion::printN0N_i(atoi(argv[1]));
+    recursion::printN0N_r(atoi(argv[1]));
+
+    // Fibonacci
+    std::cout << recursion::fibonacciNumber_r(atoi(argv[1])) << "\n";
+    std::cout << recursion::fibonacciNumber_i(atoi(argv[1])) << "\n";
+
+    std::cout << recursion::power_i(atof(argv[1]), atoi(argv[2])) << "\n";
+    std::cout << recursion::power_r(atof(argv[1]), atoi(argv[2])) << "\n";
+
+
+    std::vector<int> myVec{-98, -39, -1, 3, 7, 13, 89, 109, 133, 180, 389};
+
+    std::array<int, 11> myArray{-98, -39, -1, 3, 7, 13, 89, 109, 133, 180, 389};
+
+    std::cout << recursion::binarySearch_i<11>(myArray, atoi(argv[1])) << "\n";
+
+    std::cout << recursion::binarySearch_r(myVec, atoi(argv[1])) << "\n";
+#endif
+
+    return 0;
+}
+#endif
